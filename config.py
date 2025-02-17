03f379fb6d8404e7bbd48c98ba9e20712c4c9874
dlbuhtig4096 <null1forest@gmail.com>
2025-02-18 04:31:50 +0800

    Add config and minor improvements

--- config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    token = "" # Discord User Token
-    spawn_channel = "" # Spawn Channel ID
-    spam_channel = "" # Spam Channel ID
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from config import Config
 from banner import Banner
 from discord.ext import commands
 from colorama import Fore, Style, init
@@ -8,6 +7,10 @@ import json
 import re
 import datetime
 import os
+import base64
+
+with open('config.json', 'r', encoding='utf-8') as f:
+    Config = json.loads(f)
 
 init()
 current_datetime = datetime.datetime.now()
@@ -50,7 +53,7 @@ def start():
         while True:
             if not pause:
                 timer = random.uniform(1.5, 3)
-                await send_message(Config.spam_channel, 'Buy Premuim Version Only 10$ <@1118077275330596957>')
+                await send_message(Config["spam"], base64.b64encode(os.urandom(15)).decode())
                 await asyncio.sleep(timer)
             else:
                 await asyncio.sleep(5)
@@ -61,28 +64,28 @@ def start():
         try:
             poketwo = 716390085896962058
 
-            if message.channel.id == int(Config.spawn_channel):
+            if message.channel.id == int(Config["spawn"]):
                 if message.author.id == int(poketwo):
                     if message.embeds:
                         embed_title = message.embeds[0].title
                         if 'wild pokémon has appeared!' in embed_title:
                             timer = random.uniform(1.5, 3)
                             await asyncio.sleep(timer)
-                            await message.channel.send('<@716390085896962058> h')
+                            await message.channel.send('<@%s> h' % poketwo)
                             pause = True
 
-            if message.author.id == int(poketwo) and message.channel.id == int(Config.spawn_channel):
+            if message.author.id == int(poketwo) and message.channel.id == int(Config["spawn"]):
                 if message.content.startswith('The pokémon is '):
                     extracted_word = message.content[len('The pokémon is '):].strip('.').strip().replace('\\', '')
                     print(f'[{timestamp}] [HINT] - {Fore.YELLOW}Pokemon Hint: {Style.RESET_ALL}{extracted_word}')
                     result = find_word(words, extracted_word)
                     if result:
-                        await message.channel.send(f'<@716390085896962058> c {result}')
+                        await message.channel.send('<@%s> c %s' % (poketwo, result))
                         print(f"[{timestamp}] [HINT] - {Fore.LIGHTGREEN_EX}Search Result: {Style.RESET_ALL}{result}")
                     else:
                         print(f"[{timestamp}] [ERROR] - {Fore.RED}Pokemon Not Founded In Database{Style.RESET_ALL}")
 
-            if message.author.id == int(poketwo) and message.channel.id == int(Config.spawn_channel):
+            if message.author.id == int(poketwo) and message.channel.id == int(Config["spawn"]):
                 if message.content.startswith('Congratulations'):
                     match = re.search(r'Congratulations <@\d+>! (.+)', message.content)
                     if match:
@@ -90,6 +93,9 @@ def start():
                         print(f'[{timestamp}] [INFO] - {Fore.LIGHTGREEN_EX}{extracted_message}{Style.RESET_ALL}')
                 elif message.content.startswith('That is the wrong pokémon!'):
                     print(f'[{timestamp}] [INFO] - {Fore.RED}That is the wrong pokémon!{Style.RESET_ALL}')
+                            timer = random.uniform(3, 5)
+                            await asyncio.sleep(timer)
+                            await message.channel.send('<@%s> h' % poketwo)
 
         except Exception as e:
             print(f"[{timestamp}] [ERROR] - {Fore.RED}Error in on_message: {Style.RESET_ALL}{e}")
@@ -98,6 +104,6 @@ def start():
     with open('pokemon.json', 'r', encoding='utf-8') as f:
         words = json.load(f)
 
-    bot.run(Config.token)
+    bot.run(Config["token"])
 
 start()
