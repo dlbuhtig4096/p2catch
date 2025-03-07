1d53a2c31cb31a5b7b5e820dd639c31548f51d5b
dlbuhtig4096 <null1forest@gmail.com>
2025-03-07 16:25:14 +0800

    Add support for multiple workers

--- main.py
+++ main.py
@@ -39,8 +39,12 @@ Config = json.load(
 )
 
 POKETWO = 716390085896962058
-SPAWN = Config["spawn"]
-SPAM = Config["spam"]
+GUILD = Config["guild"]
+CATCH = Config["catch"]
+CHAT = Config["chat"]
+FARMER = Config["farmers"]
+HELPER = Config["helpers"]
+PLAYER = Config["players"]
 
 init()
 
@@ -59,92 +63,109 @@ print(
 
 now = lambda : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def helper():
-    bot = commands.Bot(command_prefix = "!")
-    pause = False
+def repeat(A):
+    while True:
+        for a in A: yield a
 
-    async def send_message(channel_id, message):
-        try:
-            channel = bot.get_channel(channel_id)
-            await channel.send(message)
-        except Exception as e:
-            print(f"{Fore.RED}Error in send_message: {e}{Style.RESET_ALL}")
+async def channel_send(bot, channel_id, message):
+    try:
+        await bot.get_channel(channel_id).send(message)
+    except Exception as e:
+        print(f"[{now()}] [ERROR] - {Fore.RED}Error in channel_send: {e}{Style.RESET_ALL}")
 
-    @bot.event
-    async def on_ready():
-        print(f"[{now()}] [INFO] - {Fore.LIGHTGREEN_EX}Logged on as {bot.user}{Style.RESET_ALL}")
+async def guild_ack(bot):
+    try:
+        await bot.get_guild(GUILD).ack()
+    except Exception as e:
+        print(f"[{now()}] [ERROR] - {Fore.RED}Error in ack: {e}{Style.RESET_ALL}")
+
+def farmer(bots):
+    if not bots: return ()
+    
+    wait = 3.0 / (len(bots) + 1.0)
+    async def main():
+        for bot in repeat(bots):
+            await asyncio.sleep(wait)
+            await channel_send(bot, CHAT, base64.b64encode(os.urandom(15)).decode())
+    
+    return (main, )
+
+def helper(bots):
+    if not bots: return ()
+    chain = repeat(bots)
+
+    wild = False
+    async def main():
         while True:
-            t = 5.0
-            if pause:
-                await send_message(SPAWN, "<@%s> h" % POKETWO)
-            else:
-                await send_message(SPAM, base64.b64encode(os.urandom(15)).decode())
-                t = random.uniform(1.5, 2.0)
-            await asyncio.sleep(t)
+            await asyncio.sleep(3.5)
+            if wild: await channel_send(next(chain), CATCH, "<@%s> h" % POKETWO)
 
-    @bot.event
+    @(bots[0]).event
     async def on_message(message):
-        nonlocal pause
-        
+        nonlocal wild
         try:
-            if message.author.id == POKETWO and message.channel.id == SPAWN:
-                content = message.content
-                if message.embeds:
-                    embed_title = message.embeds[0].title
-                    if "wild pokémon has appeared!" in embed_title: pause = True
+            if message.author.id != POKETWO or message.channel.id != CATCH: return
+            if message.embeds and message.embeds[0].title.endswith("wild pokémon has appeared!"):
+                wild = True
+                await channel_send(next(chain), CATCH, "<@%s> h" % POKETWO)
 
-                elif content.startswith("Congratulations"):
-                    if re.search(r"Congratulations <@\d+>! (.+)", content): pause = False
+            elif message.content.startswith("Congratulations"):
+                wild = False
 
         except Exception as e:
             print(f"[{now()}] [ERROR] - {Fore.RED}Error in on_message: {Style.RESET_ALL}{e}")
+            
+    return (main, )
 
-    return bot
+def player(bots):
+    if not bots: return ()
+    chain = repeat(bots)
 
-def farmer():
-    bot = commands.Bot(command_prefix = "!")
-
-    @bot.event
-    async def on_ready():
-        print(f"[{now()}] [INFO] - {Fore.LIGHTGREEN_EX}Logged on as {bot.user}{Style.RESET_ALL}")
-        return
-
-    @bot.event
+    @(bots[0]).event
     async def on_message(message):
         try:
-            if message.author.id == POKETWO and message.channel.id == SPAWN:
+            if message.author.id == POKETWO and message.channel.id == CATCH:
                 content = message.content
                 if content.startswith("The pokémon is "):
                     hint = content[15 : -1].strip().replace("\\", "")
                     print(f"[{now()}] [HINT] - {Fore.YELLOW}Pokemon Hint: {Style.RESET_ALL}{hint}")
                     result = Pokemon.find(hint)
                     if result:
-                        await message.channel.send("<@%s> c %s" % (POKETWO, result))
+                        await channel_send(next(chain), CATCH, "<@%s> c %s" % (POKETWO, result))
                         print(f"[{now()}] [HINT] - {Fore.LIGHTGREEN_EX}Search Result: {Style.RESET_ALL}{result}")
                     else:
                         print(f"[{now()}] [ERROR] - {Fore.RED}Pokemon Not Founded In Database{Style.RESET_ALL}")
 
                 elif content.startswith("Congratulations"):
                     match = re.search(r"Congratulations <@\d+>! (.+)", content)
-                    if match:
-                        hint = match.group(1)
-                        print(f"[{now()}] [INFO] - {Fore.LIGHTGREEN_EX}{hint}{Style.RESET_ALL}")
-                    await message.guild.ack()
+                    if not match: return
+                    hint = match.group(1)
+                    print(f"[{now()}] [INFO] - {Fore.LIGHTGREEN_EX}{hint}{Style.RESET_ALL}")
+                    for bot in bots: await guild_ack(bot)
 
                 elif content.startswith("That is the wrong pokémon!"):
                     print(f"[{now()}] [INFO] - {Fore.RED}That is the wrong pokémon!{Style.RESET_ALL}")
-
-
+                    
         except Exception as e:
             print(f"[{now()}] [ERROR] - {Fore.RED}Error in on_message: {Style.RESET_ALL}{e}")
 
-    return bot
+    return ()
 
 def main():
+    bots = {}
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.create_task(helper().start(Config["help"]))
-    loop.create_task(farmer().start(Config["farm"]))
+
+    for token in FARMER + HELPER + PLAYER:
+        if token in bots: continue
+        bots[token] = commands.Bot(command_prefix = "!")
+
+    tasks = []
+    for task in farmer([bots[token] for token in FARMER]): tasks.append(task)
+    for task in helper([bots[token] for token in HELPER]): tasks.append(task)
+    for task in player([bots[token] for token in PLAYER]): tasks.append(task)
+    for token, bot in bots.items(): loop.create_task(bot.start(token))
+    for task in tasks: loop.create_task(task())
     loop.run_forever()
 
 if __name__ == "__main__": main()
